bf1cb42d278a8231cf34c533458c5401d0135c58
RjkzRhen <163022810+RjkzRhen@users.noreply.github.com>
2024-04-09 11:41:25 +0300

    Update insertRecord.php

--- data/insertRecord.php
+++ data/insertRecord.php
@@ -1,24 +1,18 @@
 <?php
-include_once '../db/db.php';
 
-if (isset($_POST['submit'])) {
-    $conn = getConnection(); // Вызов функции getConnection() из файла db.php
+include '../db/db.php';
 
-    $last_name = $_POST['last_name'];
-    $first_name = $_POST['first_name'];
-    $middle_name = $_POST['middle_name'];
-    $age = $_POST['age'];
-
-    $sql = "INSERT INTO `name` (last_name, first_name, middle_name, age) VALUES (?, ?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssss", $last_name, $first_name, $middle_name, $age);
-    $stmt->execute();
-
-    header("Location: ../data/index.php");
+if (isset($_POST['update'])) {
+    $data = array(
+        'id' => $_POST['id'],
+        'last_name' => $_POST['last_name'],
+        'first_name' => $_POST['first_name'],
+        'middle_name' => $_POST['middle_name'],
+        'age' => $_POST['age']
+    );
+    insertRecord($data); 
+    header("Location: index.php");
     exit;
-
-    $stmt->close();
-    $conn->close();
 }
 ?>
 
@@ -26,19 +20,20 @@ if (isset($_POST['submit'])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Добавление пользователя</title>
+    <title>Таблица пользователей</title>
 </head>
 <body>
-<form action="insert.php" method="post">
+<form action="" method="post">
+    <input type="hidden" name="id" value="ID_записи_для_изменения">
     <label for="last_name">Фамилия:</label>
-    <input type="text" id="last_name" name="last_name" required><br>
+    <input type="text" id="last_name" name="last_name" value=""><br>
     <label for="first_name">Имя:</label>
-    <input type="text" id="first_name" name="first_name" required><br>
+    <input type="text" id="first_name" name="first_name" value=""><br>
     <label for="middle_name">Отчество:</label>
-    <input type="text" id="middle_name" name="middle_name" required><br>
+    <input type="text" id="middle_name" name="middle_name" value=""><br>
     <label for="age">Возраст:</label>
-    <input type="number" id="age" name="age" required><br>
-    <input type="submit" name="submit" value="Добавить пользователя">
+    <input type="number" id="age" name="age" value=""><br>
+    <input type="submit" name="update" value="Добавить"> <!-- Изменил значение кнопки на "Добавить" -->
 </form>
 </body>
 </html>
