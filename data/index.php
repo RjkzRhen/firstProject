d3dc366525feca75df3b110264f5bcdc2c246998
RjkzRhen <qwora1@bk.ru>
2024-05-05 18:04:41 +0300

    update

--- data/index.php
+++ data/index.php
@@ -1,30 +1,28 @@
 <?php
 include_once '../db/db.php';
 
-$minAge = isset($_GET['minAge']) ? (int)$_GET['minAge'] : 0;
+$minAge = filter_input(INPUT_GET, 'minAge', FILTER_VALIDATE_INT) ?? 0;
 $tableRows = getTableRows($minAge);
-
 
 function displayTable($tableRows) {
     echo "<table>";
     echo "<tr><th>ID</th><th>Фамилия</th><th>Имя</th><th>Отчество</th><th>Возраст</th><th>Действия</th></tr>";
     foreach ($tableRows as $row) {
-        echo "<tr>";
-        echo "<td>".$row['id']."</td>";
-        echo "<td>".$row['last_name']."</td>";
-        echo "<td>".$row['first_name']."</td>";
-        echo "<td>".$row['middle_name']."</td>";
         $ageClass = $row['age'] > 50 ? 'age-over-50' : '';
-        echo "<td class=\"". $ageClass ."\">".$row['age']."</td>";
-        echo "<td><a href='delete.php?id=".$row['id']."'>Удалить</a></td>";
-        echo "<td><a href='insertRecord.php?id=".$row['id']."'>Добавить еще</a></td>";
+        echo "<tr>";
+        echo "<td>{$row['id']}</td>";
+        echo "<td>{$row['last_name']}</td>";
+        echo "<td>{$row['first_name']}</td>";
+        echo "<td>{$row['middle_name']}</td>";
+        echo "<td class=\"$ageClass\">{$row['age']}</td>";
+        echo "<td><a href='delete.php?id={$row['id']}'>Удалить</a></td>";
+        echo "<td><a href='insertRecord.php?id={$row['id']}'>Добавить еще</a></td>";
         echo "</tr>";
     }
     echo "</table>";
 }
-
-displayTable($tableRows);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -60,5 +58,8 @@ displayTable($tableRows);
     <input type="number" id="minAge" name="minAge" value="<?= $minAge ?>">
     <input type="submit" value="Фильтровать">
 </form>
+
+<?php displayTable($tableRows); ?>
+
 </body>
 </html>
