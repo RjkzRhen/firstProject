b3553fcf8eb5945fea160b901a57b4798897cfdc
RjkzRhen <163022810+RjkzRhen@users.noreply.github.com>
2024-06-13 00:49:22 +0300

    Add files via upload

---
+++ data/DeletePage.php
@@ -0,0 +1,27 @@
+<?php
+include_once '../db/Database.php';
+
+class DeletePage {
+    private $db;
+
+    public function __construct() {
+        $this->db = new Database();
+    }
+
+    public function deleteRecordById($id) {
+        $this->db->deleteRecord($id);
+        header("Location: index.php");
+        exit;
+    }
+
+    public function handleRequest() {
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+            $this->deleteRecordById($id);
+        }
+    }
+}
+
+$page = new DeletePage();
+$page->handleRequest();
+
